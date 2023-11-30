5692c4d54d966ea2debc85a872a0db7e3056fccf
Mathias Dollenbacher <hello@mdollenbacher.net>
2023-12-01 00:44:05 +0100

    feat(student): add request to check if a student exists
    New get endpoint for checking if a student already exists in the
    database or not

--- core/src/main/java/dev/mttw/students/service/StudentService.java
+++ core/src/main/java/dev/mttw/students/service/StudentService.java
@@ -1,11 +1,20 @@
 package dev.mttw.students.service;
 
+import dev.mttw.students.repository.StudentRepository;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Getter
 @Service
+@RequiredArgsConstructor
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public boolean doesStudentExist(long id) {
+        return this.getStudentRepository().existsById(id);
+    }
 
     public int addition(String numberOne, String numberTwo) {
         int nOneInt = Integer.parseInt(numberOne);
