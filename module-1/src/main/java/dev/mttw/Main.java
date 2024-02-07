5ae5e71be13da07d404dd94d5f3e7b47c0a1c70d
Mathias Dollenbacher <hello@mdollenbacher.net>
2024-02-07 15:41:50 +0100

    feat: add useless method ^^

--- module-1/src/main/java/dev/mttw/Main.java
+++ module-1/src/main/java/dev/mttw/Main.java
@@ -4,4 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public void doNothingJustForVersionBump() {
+        System.out.println("This method does nothing and was just added to test the monorepo release strategy :)");
+    }
 }
